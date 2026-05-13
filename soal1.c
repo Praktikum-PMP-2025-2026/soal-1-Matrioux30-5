281b4800e01d84e99e02b6346b0f1967f2cc5555
Matrioux30 <izhharulmulki@gmail.com>
2026-05-13 09:35:37 +0700

    .
    .

--- soal1.c
+++ soal1.c
@@ -1,5 +1,58 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 5 – Foundation of Algorithm
+ *   Hari dan Tanggal    : Rabu, 12 mei 2026
+ *   Nama (NIM)          : Yudhi Tsabit Izhharul Mulki
+ *   Nama File           : Rabu1.c
+ *   Deskripsi           : Membuat suatu susunan parntheses () dengan jumlah keseimbangan yang sama
+ * 
+ */
 
-int main(void) {
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int N=0;
+ 
+void _printParenthesis(int pos, int n, int open, int close);
+
+void printParenthesis(int n)
+{
+    if (n > 0)
+        _printParenthesis(0, n, 0, 0);
+    return;
+}
+ 
+void _printParenthesis(int pos, int n, int open, int close)
+{
+    static char str[MAX_SIZE];
+
+
+    if (close == n) {
+        printf("%s \n", str);
+        N++;
+        return;
+    }
+    else {
+        if (open < n) {
+            str[pos] = '(';
+            _printParenthesis(pos + 1, n, open + 1, close);
+        }
+
+        if (open > close) {
+            str[pos] = ')';
+            _printParenthesis(pos + 1, n, open, close + 1);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printParenthesis(n);
+    printf("TOTAL %d", N);
+    getchar();
     return 0;
 }
+
+
+//REFERENCE CODE https://prepinsta.com/c-program/generate-all-combinations-of-balanced-parentheses/
