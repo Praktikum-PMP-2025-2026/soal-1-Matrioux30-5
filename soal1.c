0b6d228b8f22d709bd9b83e1ed9cf48e25640a89
Matrioux30 <izhharulmulki@gmail.com>
2026-05-13 09:44:22 +0700

    Handle zero input case and print total.
    .

--- soal1.c
+++ soal1.c
@@ -48,8 +48,14 @@ int main()
 {
     int n;
     scanf("%d", &n);
+    if(n == 0){
+        printf("TOTAL 1");
+        return 0;
+    }
     printParenthesis(n);
+
     printf("TOTAL %d", N);
+
     getchar();
     return 0;
 }
